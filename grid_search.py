0f3922bf8ac6a5e3142fb7335a01398562303307
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2024-09-11 21:13:33 +0530

    Create

---
+++ grid_search.py
@@ -0,0 +1,29 @@
+#author: ranja.sarkar@gmail.com
+#CLASSIFICATION task
+
+import pandas as pd
+from scipy.stats import loguniform
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import GridSearchCV
+
+#dataset for classification
+dataframe = pd.read_csv('sonar.csv')
+data = dataframe.values
+X, y = data[:, :-1], data[:, -1]
+
+#Model
+model = LogisticRegression()
+cv = RepeatedStratifiedKFold(n_splits = 10, n_repeats = 3, random_state = 1)
+
+#Define search space
+space = dict()
+space['solver'] = ['newton-cg', 'lbfgs', 'liblinear']
+space['penalty'] = ['none', 'l1', 'l2', 'elasticnet']
+space['C'] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
+search = GridSearchCV(model, space, scoring = 'accuracy', n_jobs = -1, cv = cv)
+result = search.fit(X, y)
+
+print('Best Score: %s' % result.best_score_)
+print('Best Hyperparameters: %s' % result.best_params_)
+
