556fac21f9783b9418b1340b3814f6fe548df768
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2024-09-12 10:42:06 +0530

    Update

--- gridsearch_regression.py
+++ gridsearch_regression.py
@@ -19,10 +19,12 @@ cv = RepeatedKFold(n_splits = 10, n_repeats = 3, random_state = 1)
 
 #Define search space
 space = dict()
-space['solver'] = ['svd', 'cholesky', 'lsqr', 'sag']
 space['alpha'] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
 space['fit_intercept'] = [True, False]
-space['normalize'] = [True, False]
+#space['normalize'] = [True, False]
+space['max_iter'] = [100, 200, 300]
+space['random_state'] = [1, 42]
+space['solver'] = ['svd', 'cholesky', 'lsqr', 'sag']
 
 search = GridSearchCV(model, space, scoring = 'neg_mean_absolute_error', n_jobs = -1, cv = cv)
 result = search.fit(X, y)
